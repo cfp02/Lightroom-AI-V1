6913ff930dc862aaa9a86dea6f772344a7e956c8
cfp02 <47919940+cfp02@users.noreply.github.com>
2024-04-26 21:20:02 -0400

    Init

---
+++ lrai_v1.lrdevplugin/Info.lua
@@ -0,0 +1,10 @@
+return {
+    VERSION = { major=13, minor=2, revision=0, },
+  
+    LrSdkVersion = 9.0,
+    LrSdkMinimumVersion = 4.0,
+  
+    LrToolkitIdentifier = "com.coleparks.lrai_v1",
+    LrPluginName = "lrai_v1",
+    LrPluginInfoUrl="coleparks.com",
+  }
